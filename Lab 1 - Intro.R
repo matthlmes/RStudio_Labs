60a961329b2a2757b0542551eb57a76710360650
Matthew <75327868+matthlmes@users.noreply.github.com>
2023-01-26 13:34:19 +0000

    Add files via upload

---
+++ Lab 1 - Intro.R
@@ -0,0 +1,41 @@
+# Question 1 - Sum integers
+#1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+
+# Question 2 - Subtract integers
+#95 - 10 - 32 - 67 - 87
+
+
+# Question 3 - Product of integers
+#1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10
+
+
+# Question 4
+#104 + 110034
+
+
+# Question 5
+#234 * 267
+
+# Question 6
+#1 + 2 * 3 - 5
+
+
+# Question 7
+#(1 + 2) * 3 - 5
+
+
+# Question 8
+#424/2
+
+
+# Question 9
+#25 ^ 5
+
+
+# Question 10
+?floor
+
+
+
+
